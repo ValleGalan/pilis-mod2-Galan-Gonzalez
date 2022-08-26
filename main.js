2679d6552526957438fd2858ddf62f5a292e0180
Florencia Galan <vallegalan810@gmail.com>
2022-08-26 10:56:23 -0300

    funcionalidad del clima agregada

--- main.js
+++ main.js
@@ -64,3 +64,51 @@ const getValueInput = () => {
 
 
 
+//API CLIMA - OpenWeather
+
+let longitud;
+let latitud;
+let url;
+
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(posicion=>{
+        //posiciones mias
+        //longitud= posicion.coords.longitude
+        //latitud=posicion.coords.latitude
+        //posicion de la ciudad cultural
+        latitud="-24.188654561006086";
+        longitud="-65.300252137829";
+        key= "ee3dc4f05e3eb01904cbe1629c493a51";
+        //pongo mis valores y ubicación
+        url=`https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=${key}&lang=es`;
+
+        console.log(url);
+        const getData=async()=>{
+
+            const cogerDatos=await fetch(url) 
+            const data=await cogerDatos.json() 
+            console.log(data) 
+
+            let temp=data.main.temp- 273.15 ; // temperatura en kelvin a celsius
+            
+            let date = new Date();
+            fechaActual = date.toLocaleDateString()+" "+date.getHours()+":"+date.getMinutes();
+            
+           // document.getElementById('fechaActual').src=img;
+           document.getElementById('fechaActual').textContent=" Fecha : "+ fechaActual;
+           document.getElementById('localidad').textContent= "Localidad: " +data.name;
+           document.getElementById('humedad').textContent=" Humedad: "+data.main.humidity;
+           document.getElementById('temperatura').textContent="Temperatura: "+Math. round(temp)+ "°C";
+           document.getElementById('visibilidad').textContent="Visibilidad: "+data.visibility;
+           document.getElementById('porcNubes').textContent="Pocentaje de Nubes: "+data.clouds.all;
+          
+        }
+        getData()
+    })
+}  
+
+
+ 
+
+
+
